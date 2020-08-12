1405dc91c7699b17121a95bbc3c1e84e318850b1
Vlad-Adrian Moglan <vlad.moglan@gmail.com>
2020-08-12 08:58:52 +0200

    Updated tests with expected behavior.

--- DotNetSqlitePlayground.Tests/EncryptionTest.cs
+++ DotNetSqlitePlayground.Tests/EncryptionTest.cs
@@ -4,36 +4,41 @@ using System.IO;
 
 namespace DotNetSqlitePlayground.Tests
 {
-    [TestClass]
+    [TestClass, TestCategory("Integration")]
     public class EncryptionTest
     {
         [TestMethod]
-        public void ShouldCreateAndEncryptByOpeningConnection()
+        public void WillNotEncryptDbByOpeningWithPassword()
         {
             string dataSource = EncryptionTestUtils.GetUniqueDataSource();  // does not exist
-            string password = "test";
+            string key = "key";
+
+            // trying to encrypt by creating and opening a database with a password
 
             string connectionString = new SqliteConnectionStringBuilder
             {
                 DataSource = dataSource,
-                Password = password,
+                Password = key,
                 Mode = SqliteOpenMode.ReadWriteCreate
             }.ConnectionString;
 
             using var connection = new SqliteConnection(connectionString);
-            connection.Open();
+            connection.Open();  // should encrypt at this point
 
-            // fails
+            // performing read operation to try and apply the encryption (you never know)
 
-            Assert.IsTrue(EncryptionTestUtils.IsEncrypted(dataSource));
-            Assert.IsTrue(EncryptionTestUtils.TestEncryption(dataSource, password));
+            using SqliteCommand command = connection.CreateCommand();
+            command.CommandText = "select count(*) from sqlite_master";
+            command.ExecuteScalar();
+
+            Assert.IsFalse(EncryptionTestUtils.IsEncrypted(dataSource));
         }
 
         [TestMethod]
-        public void ShouldCreateAndEncryptByExecutingKeyOperation()
+        public void WillNotEncryptDbOnKeyOperation()
         {
             string dataSource = EncryptionTestUtils.GetUniqueDataSource();  // does not exist
-            string password = "test";
+            string key = "key";
 
             string connectionString = new SqliteConnectionStringBuilder
             {
@@ -41,49 +46,28 @@ namespace DotNetSqlitePlayground.Tests
                 Mode = SqliteOpenMode.ReadWriteCreate
             }.ConnectionString;
 
-            using var connection = new SqliteConnection(connectionString);
-            connection.Open();
-
-            EncryptionTestUtils.ExecuteKeyQuery(connection, password, false);
-
-            // fails
-
-            Assert.IsTrue(EncryptionTestUtils.IsEncrypted(dataSource));
-            Assert.IsTrue(EncryptionTestUtils.TestEncryption(dataSource, password));
-        }
-
-        [TestMethod]
-        public void ShouldCreateAndEncryptDbByExecutingKeyThenRekeyOperation()
-        {
-            string dataSource = EncryptionTestUtils.GetUniqueDataSource();  // does not exist
-            string password = "test";
-
-            string connectionString = new SqliteConnectionStringBuilder
-            {
-                DataSource = dataSource,
-                Mode = SqliteOpenMode.ReadWriteCreate
-            }.ConnectionString;
-            
             // trying to encrypt
 
             using var connection = new SqliteConnection(connectionString);
             connection.Open();
 
-            EncryptionTestUtils.ExecuteKeyQuery(connection, password, false);
-            EncryptionTestUtils.ExecuteKeyQuery(connection, password);
+            EncryptionTestUtils.ExecuteKeyQuery(connection, key, false);    // should encrypt at this point
 
-            // succeeds
+            // performing read operation to try and apply the encryption (you never know)
 
-            Assert.IsTrue(EncryptionTestUtils.IsEncrypted(dataSource));
-            Assert.IsTrue(EncryptionTestUtils.TestEncryption(dataSource, password));
+            using SqliteCommand command = connection.CreateCommand();
+            command.CommandText = "select count(*) from sqlite_master";
+            command.ExecuteScalar();
+
+            Assert.IsFalse(EncryptionTestUtils.IsEncrypted(dataSource));
         }
 
         [TestMethod]
-        public void ShouldCreateAndReEncryptDb()
+        public void WillCreateEncryptAndReEncryptDb()
         {
             string dataSource = EncryptionTestUtils.GetUniqueDataSource();  // does not exist
-            string initialPassword = "test";
-            string newPassword = "something else";
+            string initialKey = "initial key";
+            string newKey = "new key";
 
             string connectionString = new SqliteConnectionStringBuilder
             {
@@ -97,16 +81,24 @@ namespace DotNetSqlitePlayground.Tests
             {
                 connection.Open();
 
-                EncryptionTestUtils.ExecuteKeyQuery(connection, initialPassword, false);
-                EncryptionTestUtils.ExecuteKeyQuery(connection, initialPassword);
+                /*
+                 * for some reason executing the 'key' pragma does not encrypt the database;
+                 * the 'key' statement must be followed by a 'rekey'.
+                 */
+
+                EncryptionTestUtils.ExecuteKeyQuery(connection, initialKey, false);
+                EncryptionTestUtils.ExecuteKeyQuery(connection, initialKey);
             }
+
+            Assert.IsTrue(EncryptionTestUtils.IsEncrypted(dataSource));
+            Assert.IsTrue(EncryptionTestUtils.TestEncryption(dataSource, initialKey));
 
             // re-encryption
 
             connectionString = new SqliteConnectionStringBuilder
             {
                 DataSource = dataSource,
-                Password = initialPassword,
+                Password = initialKey,
                 Mode = SqliteOpenMode.ReadWrite
             }.ConnectionString;
 
@@ -114,21 +106,23 @@ namespace DotNetSqlitePlayground.Tests
             {
                 connection.Open();
 
-                EncryptionTestUtils.ExecuteKeyQuery(connection, newPassword);
+                EncryptionTestUtils.ExecuteKeyQuery(connection, newKey);
             }
 
-            // succeeds
-
             Assert.IsTrue(EncryptionTestUtils.IsEncrypted(dataSource));
-            Assert.IsTrue(EncryptionTestUtils.TestEncryption(dataSource, newPassword));
-            Assert.IsFalse(EncryptionTestUtils.TestEncryption(dataSource, initialPassword));
+            Assert.IsTrue(EncryptionTestUtils.TestEncryption(dataSource, newKey));
+            Assert.IsFalse(EncryptionTestUtils.TestEncryption(dataSource, initialKey));
         }
 
+        /// <summary>
+        /// PRAGMA key = '{some key}' is the first operation that needs to be executed on the database; as such
+        /// a database that is populated cannot be encrypted.
+        /// </summary>
         [TestMethod]
-        public void ShouldEncryptExistingPopulatedDb()
+        public void WillNotEncryptExistingPopulatedDb()
         {
             string dataSource = EncryptionTestUtils.GetUniqueDataSource();  // does not exist
-            string password = "test";
+            string key = "key";
 
             string connectionString = new SqliteConnectionStringBuilder
             {
@@ -162,20 +156,17 @@ namespace DotNetSqlitePlayground.Tests
 
             Assert.IsTrue(File.Exists(dataSource));
 
-            // encryption
+            // trying to encrypt
 
             using (var connection = new SqliteConnection(connectionString))
             {
                 connection.Open();
 
-                EncryptionTestUtils.ExecuteKeyQuery(connection, password, false);
-                EncryptionTestUtils.ExecuteKeyQuery(connection, password);
+                EncryptionTestUtils.ExecuteKeyQuery(connection, key, false);
+                EncryptionTestUtils.ExecuteKeyQuery(connection, key);
             }
 
-            // fails
-
-            Assert.IsTrue(EncryptionTestUtils.IsEncrypted(dataSource));
-            Assert.IsTrue(EncryptionTestUtils.TestEncryption(dataSource, password));
+            Assert.IsFalse(EncryptionTestUtils.IsEncrypted(dataSource));
         }
     }
 }
